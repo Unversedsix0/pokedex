d727b9199e907af9687147e71b349a909bdf500a
Unversedsix0 <gaba.olipires@gmail.com>
2023-01-05 10:14:58 -0300

    feat: add function getPokemons

--- src/components/Pokedex/Pokedex.js
+++ src/components/Pokedex/Pokedex.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import { styles } from './styles';
+import React from "react";
+import { Text, View } from "react-native";
+import { styles } from "./styles";
 
 export const Pokedex = () => {
   return (
     <View style={styles.container}>
+      <View style={styles.pokedexHeader}>
         <Text>Pokedex</Text>
-        <Text>Pagination</Text>
+        <Text>Pagination:</Text>
+      </View>
     </View>
   );
-}
+};
---
+++ src/components/Pokedex/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+
+  },
+  pokedexHeader:{
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center',
+    padding:12
+  },
+  pokedexGrid:{
+    d
+  }
+});
