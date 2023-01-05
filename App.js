653f6f92ba801b710f0033646d2c836837372dd1
Unversedsix0 <gaba.olipires@gmail.com>
2023-01-05 09:37:34 -0300

    feat: add @react-navigation/material-bottom-tabs react-native-paper react-native-vector-icons

--- App.js
+++ App.js
@@ -1,18 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
-import { Favorites, Home } from './src/screens';
-
+import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { Favorites, Home } from "./src/screens";
 
 
 export default function App() {
+  const Tab = createMaterialBottomTabNavigator();
 
   return (
-    
-    
-      <Favorites/>
-      
-   
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="My Favorites" component={Favorites} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-
