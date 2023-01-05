033c800d2d9c7d34de84ac2c25ea9fc6db69012f
Unversedsix0 <gaba.olipires@gmail.com>
2023-01-05 09:51:36 -0300

    feat: add bottom tab navigation

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { Favorites, Home } from "./src/screens";
 
 
@@ -11,8 +12,28 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="My Favorites" component={Favorites} />
+        <Tab.Screen 
+        name="Home" 
+        component={Home} 
+        options={{
+          tabBarLabel: 'Pokedex',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}
+        />
+
+        <Tab.Screen 
+        name="Favorites" 
+        component={Favorites} 
+        options={{
+          tabBarLabel: 'My Favorites',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="heart" color={color} size={26} />
+          ),
+        }}
+  
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
